0e879c0df971fc5aeb6fd651e30a2aa8fc1dc255
Alexander Stolzoff <alexanderstolzoff@Alexanders-MacBook-Pro.local>
2020-06-16 01:42:36 -0700

    navbar and routing

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Route} from "react-router-dom"
+
+export default function SingleRoom() {
+    
+    return (
+        <div>
+            Welcome to Single Room Page 
+        </div>
+    )
+}
