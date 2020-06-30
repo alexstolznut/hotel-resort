576272e72ef4e71cb38606b25c453b3dab015c28
Alexander Stolzoff <alexstolznut@hotmail.com>
2020-06-30 00:09:58 -0700

    single room

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 
 
 export default function Rooms() {
+    
     return (
         <div>
             <Hero hero="roomsHero">
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import {Route} from "react-router-dom"
+import React, { useContext } from 'react'
+import {RoomContext} from '../context';
+import {useParams} from "react-router-dom";
+import SingleRoomComp from "../components/SingleRoom"
+
 
 export default function SingleRoom() {
-    
+    let slug = useParams().slug;
+    const {rooms} = useContext(RoomContext)
+    console.log(rooms.find(rooms=>rooms.slug===slug))
+    // let SingleRooms = rooms.find(rooms=>rooms.slug===slug)
+    let SingleRoom = rooms.map(SingleRoom=>{
+        if(SingleRoom.slug.match(slug)) return <SingleRoomComp room={SingleRoom}></SingleRoomComp>
+    })
     return (
         <div>
-            Welcome to Single Room Page 
+            {SingleRoom}
         </div>
     )
 }
